16d2a33181edabda6f9c33b72e917d749177c6fc
op4804 <op4804@gmail.com>
2022-01-02 18:51:39 +0900

    work at 2022-01-02
    work at main.py

--- TextPoker/System.py
+++ TextPoker/System.py
@@ -1,14 +1,16 @@
-def initPoker():    
+def initPoker(playerhands:list):
+    for i in range(len(playerhands)):
+        playerhands[i] = []    
     #머니 
     #내덱
     #상대덱
     #전체댁
     #
-    return 0
+    return playerhands
 def initGame():
     return 0
 
-def resetDeck(deck:list):
+def resetDeck():
     reseted = ["HA","H2","H3","H4","H5","H6","H7","H8","H9","H10","HJ","HQ","HK",
 "SA","S2","S3","S4","S5","S6","S7","S8","S9","S10","SJ","SQ","SK",
 "DA","D2","D3","D4","D5","D6","D7","D8","D9","D10","DJ","DQ","DK",
@@ -16,6 +18,10 @@ def resetDeck(deck:list):
 ]
     return reseted
 
+def showCurrenthands(playerhands:list,field:list):
+    print('field: ',field)
+    print('yourhand: ',playerhands[0])
+    return 0
 def resetPocker():
     return 0
 
@@ -29,4 +35,8 @@ def checkWinner():
     return 0
 
 def calcMoney():
+    return 0
+
+def waitInput():
+    input()
     return 0
--- TextPoker/main.py
+++ TextPoker/main.py
@@ -4,20 +4,40 @@ import System
 
 
 
-player_num = input("How many players do you want to play?: ")
+'''
+player_num = int(input("How many players do you want to play?: \n(please input only numbers)\n"))
 
-
-player_hand = []
-enemy_hand = []
-
+'''
 players_hands = []
+player_num = 2
+for i in range(player_num):
+    players_hands.append([])
 
-
-System.initPoker()
+''' 
+here is the init points
+'''
+System.initPoker(players_hands)
 deck = System.resetDeck()
+field = []
 
+players_hands[0].append(deck.pop(random.randrange(1,len(deck))))
 
+players_hands[1].append(deck.pop(random.randrange(1,len(deck))))
 
+players_hands[0].append(deck.pop(random.randrange(1,len(deck))))
 
-player_hand.append(deck.pop(random.randrange(1,len(deck))))
-player_hand.append(deck.pop(random.randrange(1,len(deck))))
+players_hands[1].append(deck.pop(random.randrange(1,len(deck))))
+
+field.append(deck.pop(random.randrange(1,len(deck))))
+field.append(deck.pop(random.randrange(1,len(deck))))
+field.append(deck.pop(random.randrange(1,len(deck))))
+
+System.showCurrenthands(players_hands,field)
+System.waitInput()
+field.append(deck.pop(random.randrange(1,len(deck))))
+System.showCurrenthands(players_hands,field)
+System.waitInput()
+field.append(deck.pop(random.randrange(1,len(deck))))
+System.showCurrenthands(players_hands,field)
+System.waitInput()
+
